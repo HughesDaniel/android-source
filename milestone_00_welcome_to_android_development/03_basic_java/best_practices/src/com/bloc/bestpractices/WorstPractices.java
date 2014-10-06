4948efe74b4599e77debabeca602cd336a29226e
HughesDaniel <danielh@toxicshock.biz>
2014-10-06 16:13:14 -0700

    made my pretty code even prettier!

--- milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -9,28 +9,28 @@ public class WorstPractices extends Object {
 	// EDIT BELOW
 
 	public static void main(String [] args) {
-		int MAGIC_NUMBER = WorstPractices.animals(false);
-		MAGIC_NUMBER *= 5;
-		if (MAGIC_NUMBER > 18) {
-			while(MAGIC_NUMBER > 0) {
-				MAGIC_NUMBER--;
+		int magicNumber = WorstPractices.generateMagicNumber(false);
+		magicNumber *= 5;
+		if (magicNumber > 18) {
+			while(magicNumber > 0) {
+				magicNumber--;
 			}
 		}
 	}
 
-	/* animals
-	* this method takes in a single parameter, yeswellheresthething. Using a very 
-	* elaborate and complex algorithm, it calculate a magic number
-	* yeswellheresthething: a seed which helps generate the magic number
+	/* generateNumber
+	* this method takes in a single parameter, a boolean. Using a very 
+	* elaborate and complex algorithm, it calculate a magic number.
+	* bool: a seed which helps generate the magic number
 	* returns: a magical number */
-	private static int animals(boolean yeswellheresthething) {
+	private static int generateMagicNumber(boolean bool) {
 		// Start off with one of these
-		int aInt = yeswellheresthething ? 34 : 21;
-		float sparklesfairy = .5f;
-		for (int brown = 0; brown < aInt; brown++) { 
-			sparklesfairy *= aInt;
+		int baseInt = bool ? 34 : 21;
+		float fairyNumber = .5f; // This is the number that makes the magic
+		for (int i = 0; i < baseInt; i++) { 
+			fairyNumber *= baseInt;
 		}	 
-		return (int) sparklesfairy * aInt;
+		return (int) fairyNumber * baseInt;
 	}
 
 	// STOP EDITING
