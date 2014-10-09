749f5d8b8cc4f2cc20185b3719b682d2315f1d77
HughesDaniel <danielh@toxicshock.biz>
2014-10-08 20:33:01 -0700

    moved file created to seperate thread class

--- milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
+++ milestone_01_java/02_nuance/threads/src/com/bloc/threads/ImageGetter.java
@@ -1,8 +1,44 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
+
+	URL url;
+	boolean launch;
+
+	public ImageGetter(URL url, boolean launch) {
+		this.url = url;
+		this.launch = launch;
+	}
+
 	@Override
 	public void run() {
-		// TODO
+		
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			};
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if (launch) {
+				if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+					Runtime.getRuntime().exec("open google_logo.png");
+				} else {
+					Runtime.getRuntime().exec("google_logo.png");
+				}
+			}	
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
