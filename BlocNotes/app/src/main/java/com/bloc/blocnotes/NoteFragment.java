d2c2650a7523f594280845a009ed0d58def03d01
HughesDaniel <danielh@toxicshock.biz>
2014-10-09 16:15:23 -0700

    added views to layout and inflated layout

--- BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
+++ BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -1,14 +1,12 @@
 package com.bloc.blocnotes;
 
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 /**
  * Created by Daniel on 10/9/2014.
@@ -20,21 +18,11 @@ public class NoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceSTate) {
-
         Log.d(TAG, "entered onCreateView()");
 
-        Context context = getActivity().getApplicationContext();
+        EditText editText = (EditText) getActivity().findViewById(R.id.et_note);
 
-        LinearLayout linearLayout = new LinearLayout(context);
-        EditText editText = new EditText(context);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        linearLayout.addView(editText, params);
-
-
-        return linearLayout;
-        //return inflater.inflate(R.layout.fragment_note, container, false);
+        return inflater.inflate(R.layout.fragment_note, container, false);
     }
 
 
