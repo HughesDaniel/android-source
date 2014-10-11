44cd241602ba40e611b0f3139a38a01fec3e45c4
HughesDaniel <danielh@toxicshock.biz>
2014-10-10 17:13:38 -0700

    working on savedinstance state

--- BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
+++ BlocNotes/app/src/main/java/com/bloc/blocnotes/NoteFragment.java
@@ -14,17 +14,34 @@ import android.widget.EditText;
 public class NoteFragment extends Fragment {
 
     private static final String TAG = ".NoteFragment.java";
+    private static final String STRING_KEY = "com.bloc.blocnotes.notefragment.stringkey";
 
-    @Override
+    EditText editText;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceSTate) {
+                             Bundle bundle) {
         Log.d(TAG, "entered onCreateView()");
 
-        EditText editText = (EditText) getActivity().findViewById(R.id.et_note);
+        if (bundle != null && bundle.getString(STRING_KEY) != null) {
+            String s = bundle.getString(STRING_KEY);
+            Log.d(TAG, "savedinstance state wasnt null: " + s);
+        }
+
+        editText = (EditText) getActivity().findViewById(R.id.et_note);
+
+
 
         return inflater.inflate(R.layout.fragment_note, container, false);
     }
 
+    @Override
+    public void  onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String s = editText.getText().toString();
+        Log.d(TAG, s);
+        outState.putString(STRING_KEY, s);
+    }
 
 }
 
